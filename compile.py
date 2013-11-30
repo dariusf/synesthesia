731dd97d696efdd5bb0f9b7a3b79775d2216a57f
Darius Foo <darius.foo.tw@gmail.com>
2013-11-30 16:54:59 +0800

    Refactoring: created HighlightingScheme class

--- compile.py
+++ compile.py
@@ -19,9 +19,8 @@ def read_file(filepath):
 def strip_non_alpha(string):
 	return re.sub("[^A-Za-z]+", "", string)
 
-def get_file_name(filepath):
-	temp = filepath.split("\\")
-	return temp[len(temp)-1].split(".")[0]
+def remove_ext(filename):
+	return filename.split(".")[0]
 
 def get_file_name_with_ext(filepath):
 	temp = filepath.split("\\")
@@ -30,13 +29,26 @@ def get_file_name_with_ext(filepath):
 def concat_string_list(lst):
 	return ''.join(lst)
 
+def read_default_settings(view):
+	colour_scheme_path = re.sub("Packages", sublime.packages_path(), view.settings().get('color_scheme'))
+
+	if (os.path.exists(colour_scheme_path)):
+		settings_block = re.compile(r"[ \t]+<dict>\s+<key>settings</key>[\s\w></#]+</dict>")
+		settings_text = read_file(colour_scheme_path)
+		match = settings_block.search(settings_text)
+		if match:
+			default_colours = match.group(0)
+			return default_colours
+
+	return None
+
 class SynesthesiaCompileCommand(sublime_plugin.WindowCommand):
     def run(self, cmd = []):
     	# prepare input file paths
 		path = cmd[0] if len(cmd) > 0 else self.window.active_view().file_name()
 		filepath = os.path.abspath(path)
 		filename = get_file_name_with_ext(filepath)
-		themename = get_file_name(filepath)
+		themename = remove_ext(filename)
 
 		# load input file
 		try:
@@ -45,10 +57,34 @@ class SynesthesiaCompileCommand(sublime_plugin.WindowCommand):
 			sublime.status_message("%s is not a vaild JSON file." % filename)
 			return
 
+		hs = HighlightingScheme(entries)
+
+		default_colours = read_default_settings(self.window.active_view())
+		if default_colours:
+			hs.default_colours = default_colours
+
+		hs.save(themename)
+
+class HighlightingScheme():
+	"""
+	A highlighting scheme consists of:
+
+	- a syntax definition
+	- a colour theme
+	- a settings file
+
+	"""
+
+	default_colours = templates.default_colours_template
+
+	def __init__(self, data):
+		self.data = data
+
+	def save(self, themename):
 		# initialization
 		patterns = []
 		theme_scopes = []
-		pattern_map = entries["patterns"]
+		pattern_map = self.data["patterns"]
 		count = 0
 
 		# generate syntax and theme files
@@ -71,17 +107,6 @@ class SynesthesiaCompileCommand(sublime_plugin.WindowCommand):
 		patterns = concat_string_list(patterns)
 		theme_scopes = concat_string_list(theme_scopes)
 
-		# read settings from current colour scheme
-		colour_scheme_path = re.sub("Packages", sublime.packages_path(), self.window.active_view().settings().get('color_scheme'))
-		default_colours = templates.default_colours_template
-
-		if (os.path.exists(colour_scheme_path)):
-			settings_block = re.compile(r"[ \t]+<dict>\s+<key>settings</key>[\s\w></#]+</dict>")
-			settings_text = read_file(colour_scheme_path)
-			match = settings_block.search(settings_text)
-			if match:
-				default_colours = match.group(0)
-
 		# produce output files
 		package_directory = sublime.packages_path() + "\\Synesthesia\\"
 		scope_filename = package_directory + themename + ".tmLanguage"
@@ -89,7 +114,7 @@ class SynesthesiaCompileCommand(sublime_plugin.WindowCommand):
 		settings_filename = package_directory + themename + ".sublime-settings"
 		write_file(scope_filename, templates.scope_template % (themename, patterns, themename, uuid.uuid4()))
 		print "Written to %s." % scope_filename
-		write_file(theme_filename, templates.theme_template % (themename, default_colours, theme_scopes, uuid.uuid4()))
+		write_file(theme_filename, templates.theme_template % (themename, self.default_colours, theme_scopes, uuid.uuid4()))
 		print "Written to %s." % theme_filename
 		write_file(settings_filename, templates.default_settings_template % themename)
 		print "Written to %s." % settings_filename
