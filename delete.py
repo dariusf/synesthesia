f995037ebbe41f218757d96b76105c0b069d0d18
Darius Foo <darius.foo.tw@gmail.com>
2015-06-03 22:59:06 +0800

    Added safety check for scheme deletion
    Schemes won't be deleted if we can't find all essential files (language
    def, theme, and settings) in the same directory. This is to reduce the
    chance of erroneously identifying a scheme as removable, since
    Packages/User is a public directory.

--- delete.py
+++ delete.py
@@ -5,21 +5,33 @@ from . import compile
 def extract_syntax_name(path):
 	return os.path.split(path)[1].split(".")[0]
 
+def essential_filetypes_present(path, filetypes, scheme_name):
+	for filetype in filetypes:
+		ft_path = os.path.join(path, filetype % scheme_name)
+		if not (os.path.exists(ft_path) and os.path.isfile(ft_path)):
+			return False
+	return True
+
 class SynesthesiaDeleteCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		window = self.window
 
-		filetypes = ["%s.sublime-settings", "%s.tmLanguage", "%s.tmLanguage.cache", "%s.tmTheme", "%s.tmTheme.cache"]
+		essential_filetypes = ["%s.sublime-settings", "%s.tmLanguage", "%s.tmTheme"]
 
 		# Find .tmLanguage files
 		files = [f for f in os.listdir(compile.SYNESTHESIA_OUTPUT_PATH) if os.path.isfile(os.path.join(compile.SYNESTHESIA_OUTPUT_PATH, f))]
 		files = sorted([f for f in files if f.endswith('.tmLanguage')])
 		without_extensions = [re.sub('.tmLanguage', '', f) for f in files]
 
+		# Ensure that all essential filetypes are present for safety reasons.
+		# If we can't find all those files, we don't consider the scheme for deletion.
+
+		without_extensions = [x for x in without_extensions if essential_filetypes_present(compile.SYNESTHESIA_OUTPUT_PATH, essential_filetypes, x)]
+
 		def done(which):
 			# Do nothing in the event of quick panel cancellation
 			if not (which == -1):
-				nonlocal without_extensions, filetypes
+				nonlocal without_extensions, essential_filetypes
 				which = without_extensions[which]
 
 				# Check if open views are using the syntax file that is going to be deleted.
@@ -31,8 +43,10 @@ class SynesthesiaDeleteCommand(sublime_plugin.WindowCommand):
 
 				# Delete files
 
+				all_filetypes = essential_filetypes + ["%s.tmLanguage.cache", "%s.tmTheme.cache"]
 				some_files_deleted = False
-				for filepath in filetypes:
+
+				for filepath in all_filetypes:
 					filepath = os.path.join(compile.SYNESTHESIA_OUTPUT_PATH, filepath % which)
 					if os.path.exists(filepath):
 						os.remove(filepath)
