1fab776d4920ffd21c11253e123c0a13a6f0b043
Darius Foo <darius.foo.tw@gmail.com>
2015-06-03 22:59:06 +0800

    Updated deletion paths

--- delete.py
+++ delete.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import os, re
+from . import compile
 
 def extract_syntax_name(path):
 	return os.path.split(path)[1].split(".")[0]
@@ -8,37 +9,38 @@ class SynesthesiaDeleteCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		window = self.window
 
+		filetypes = ["%s.sublime-settings", "%s.tmLanguage", "%s.tmLanguage.cache", "%s.tmTheme", "%s.tmTheme.cache"]
+
 		# Find .tmLanguage files
-		directory = os.path.join(sublime.packages_path(), "synesthesia")
-		files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+		files = [f for f in os.listdir(compile.SYNESTHESIA_OUTPUT_PATH) if os.path.isfile(os.path.join(compile.SYNESTHESIA_OUTPUT_PATH, f))]
 		files = sorted([f for f in files if f.endswith('.tmLanguage')])
 		without_extensions = [re.sub('.tmLanguage', '', f) for f in files]
 
 		def done(which):
+			# Do nothing in the event of quick panel cancellation
 			if not (which == -1):
-				nonlocal without_extensions
+				nonlocal without_extensions, filetypes
 				which = without_extensions[which]
 
-				# Check if open views are using the syntax file that is going to be deleted
+				# Check if open views are using the syntax file that is going to be deleted.
+				# If so, unset them.
 				for v in self.window.views():
 					current_def = extract_syntax_name(v.settings().get('syntax'))
 					if (current_def == which):
 						v.set_syntax_file("Packages/Text/Plain text.tmLanguage")
 
-				# Delete the files
-				package_directory = os.path.join(sublime.packages_path(), "synesthesia")
-				files = ["%s.sublime-settings", "%s.tmLanguage", "%s.tmLanguage.cache", "%s.tmTheme", "%s.tmTheme.cache"]
-				files_deleted = False
+				# Delete files
 
-				for filepath in files:
-					filepath = os.path.join(package_directory, filepath % which)
+				some_files_deleted = False
+				for filepath in filetypes:
+					filepath = os.path.join(compile.SYNESTHESIA_OUTPUT_PATH, filepath % which)
 					if os.path.exists(filepath):
 						os.remove(filepath)
-						files_deleted = True
+						some_files_deleted = True
 					else:
 						print("File not found; can't be deleted: %s" % filepath)
 
-				if files_deleted:
+				if some_files_deleted:
 					sublime.status_message("Highlighting scheme %s removed." % which)
 				else:
 					sublime.status_message("Highlighting scheme %s not found." % which)
