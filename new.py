10329a469dd8d70879d6822d08d05e01328e7814
Darius Foo <darius.foo.tw@gmail.com>
2015-06-03 23:00:36 +0800

    Updated new paths

--- new.py
+++ new.py
@@ -1,11 +1,12 @@
 import sublime, sublime_plugin
 import os, tempfile, webbrowser
 from . import templates
+from . import compile
 
 class SynesthesiaNewCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		v = self.window.new_file()
-		v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'synesthesia'))
+		v.settings().set('default_dir', compile.SYNESTHESIA_OUTPUT_PATH)
 		v.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
 
 		v.run_command("insert_snippet", {"contents": templates.new_file})
@@ -13,7 +14,7 @@ class SynesthesiaNewCommand(sublime_plugin.WindowCommand):
 class SynesthesiaSampleCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		v = self.window.new_file()
-		v.settings().set('default_dir', os.path.join(sublime.packages_path(), 'synesthesia'))
+		v.settings().set('default_dir', compile.SYNESTHESIA_OUTPUT_PATH)
 		v.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
 
 		v.run_command("insert_snippet", {"contents": templates.sample_file})
